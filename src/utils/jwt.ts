fdaef8c772d8e876a3fc6be0364f26fc7e1ed527
md-maruf-bellah <marufbellah5511507@gmail.com>
2026-07-17 02:07:20 +0600

    env file promlem solved

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -11,9 +11,11 @@ export function signToken(payload: JwtPayload): string {
   const options: SignOptions = {
     expiresIn: env.jwtExpiresIn as SignOptions["expiresIn"],
   };
+  if (!env.jwtSecret) throw new Error("JWT secret is not defined");
   return jwt.sign(payload, env.jwtSecret, options);
 }
 
 export function verifyToken(token: string): JwtPayload {
+  if (!env.jwtSecret) throw new Error("JWT secret is not defined");
   return jwt.verify(token, env.jwtSecret) as JwtPayload;
 }
